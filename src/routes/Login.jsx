f8807947d0d7cd904fe2bb5307f91a03b5afd5c5
Nicolas <haubrichtnicolas@gmail.com>
2024-09-23 08:42:03 -0300

    Criando arquivos iniciais do nav

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -17,8 +17,8 @@ const Login = () => {
             alert('Login realizado com sucesso!')
 
             let token =
-             Math.random().toString(16).substring(2) +
-             Math.random().toString(16).substring(2)
+                Math.random().toString(16).substring(2) +
+                Math.random().toString(16).substring(2)
 
             sessionStorage.setItem('user', 'Admin');
             sessionStorage.setItem('password', token);
@@ -27,25 +27,25 @@ const Login = () => {
         }
     }
 
-  return (
-    <>
-    <LoginStyle>
-        <section className="pag-login">
-            <form className='container-login' onSubmit={handleLogin}>
-                <div className='inputs-login'>
-                    <p className='p-login'>Usuário</p>
-                    <input type='text' placeholder='Usuário' className='input-login' ref={user}/>
-                </div>
-                <div className='inputs-login'>
-                    <p className='p-login'>Senha</p>
-                    <input type="password" placeholder='Senha' className='input-login' ref={password} />
-                </div>
-                <button type='submit' className='button-login'>Enviar</button>
-            </form>
-        </section>
-    </LoginStyle>
-    </>
-  )
+    return (
+        <>
+        <LoginStyle>
+            <section className="pag-login">
+                <form className='container-login' onSubmit={handleLogin}>
+                    <div className='inputs-login'>
+                        <p className='p-login'>Usuário</p>
+                        <input type='text' placeholder='Usuário' className='input-login' ref={user}/>
+                    </div>
+                    <div className='inputs-login'>
+                        <p className='p-login'>Senha</p>
+                        <input type="password" placeholder='Senha' className='input-login' ref={password} />
+                    </div>
+                    <button type='submit' className='button-login'>Enviar</button>
+                </form>
+            </section>
+        </LoginStyle>
+        </>
+    )
 }
 
 export default Login
